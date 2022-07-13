51b07fe5232c544f07b66f16514709331ac7596f
Nick Wogan <nicholaswogan@gmail.com>
2022-07-13 11:12:32 -0700

    added radiative transfer type which does solar and ir radiative trasnfer

--- src/radtran/clima_radtran.f90
+++ src/radtran/clima_radtran.f90
@@ -2,20 +2,16 @@ module clima_radtran
   use clima_const, only: dp, s_str_len
   use clima_radtran_types, only: OpticalProperties, RadiateXSWrk, RadiateZWrk
   implicit none
-  
-  ! initialize with
-  ! settings.yaml, sun.txt, and data dir, species names
-  ! 
-  ! inputs are
-  ! surface_albedo, u0, T, densities, dz
-  ! ouptuts are
-  ! fluxes (fup, fdn)
+  private
+
+  public :: RadtranIR ! IR radiative transfer only
+  public :: Radtran ! IR and solar radiative transfer
   
   type :: ClimaRadtranWrk
     
     ! work arrays
-    type(RadiateXSWrk) :: rx_ir
-    type(RadiateZWrk) :: rz_ir
+    type(RadiateXSWrk) :: rx
+    type(RadiateZWrk) :: rz
     
     !! (nz+1,nw) mW/m2/Hz in each wavelength bin
     !! at the edges of the vertical grid
@@ -27,6 +23,10 @@ module clima_radtran
     real(dp), allocatable :: fdn_n(:) 
     
   end type
+
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  !!! IR Radiative Transfer !!!
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   
   type :: RadtranIR
   
@@ -36,36 +36,63 @@ module clima_radtran
     ! number of layers
     integer :: nz
   
-    !!! Optical properties !!!
+    ! Optical properties
     type(OpticalProperties) :: ir
     
+    ! work
     type(ClimaRadtranWrk) :: wrk_ir
     
   contains
     procedure :: radiate => RadtranIR_radiate
     procedure :: OLR => RadtranIR_OLR
   end type
-  
-  type, extends(RadtranIR) :: Radtran
 
-    !!! Optical properties !!!
-    type(OpticalProperties) :: sol
-  
-    real(dp) :: diurnal_fac = 0.5_dp
-    real(dp), allocatable :: photons_sol(:) ! (nw) mW/m2/Hz in each bin  
-    
-    type(ClimaRadtranWrk) :: wrk_sol
-    
-  end type
-  
   interface RadtranIR
-    module procedure :: create_RadtranIR
-    module procedure :: create_RadtranIR_
+    module procedure :: create_RadtranIR_1
+    module procedure :: create_RadtranIR_2
+  end interface
+  
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  !!! IR and Solar Radiative Transfer !!!
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  type :: Radtran
+
+    integer :: ng
+    character(s_str_len), allocatable :: species_names(:) ! (ng) copy of species names
+  
+    ! number of layers
+    integer :: nz
+  
+    !!! Optical properties !!!
+    type(OpticalProperties) :: ir
+    type(OpticalProperties) :: sol
+    
+    real(dp) :: diurnal_fac = 0.5_dp
+    real(dp) :: solar_zenith
+    real(dp) :: surface_albedo
+    real(dp), allocatable :: photons_sol(:) ! (nw) mW/m2/Hz in each bin  
+
+    type(ClimaRadtranWrk) :: wrk_ir
+    type(ClimaRadtranWrk) :: wrk_sol
+    real(dp), allocatable :: f_total(:)
+    
+  contains
+    procedure :: radiate =>Radtran_radiate
+  end type
+
+  interface Radtran
+    module procedure :: create_Radtran_1
+    module procedure :: create_Radtran_2
   end interface
   
 contains
+
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  !!! IR Radiative Transfer !!!
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   
-  function create_RadtranIR(datadir, settings_f, nz, err) result(rad)
+  function create_RadtranIR_1(datadir, settings_f, nz, err) result(rad)
     use clima_types, only: ClimaSettings
     
     character(*), intent(in) :: datadir
@@ -85,12 +112,12 @@ contains
       return
     endif
     
-    rad = create_RadtranIR_(datadir, s%species, s, nz, err)
+    rad = create_RadtranIR_2(datadir, s%species, s, nz, err)
     if (allocated(err)) return
     
   end function
   
-  function create_RadtranIR_(datadir, species_names, s, nz, err) result(rad)
+  function create_RadtranIR_2(datadir, species_names, s, nz, err) result(rad)
     use clima_radtran_types, only: FarUVOpticalProperties, SolarOpticalProperties, IROpticalProperties
     use clima_types, only: ClimaSettings
     
@@ -119,8 +146,8 @@ contains
     if (allocated(err)) return
     
     ! work arrays
-    rad%wrk_ir%rx_ir = RadiateXSWrk(rad%ir, nz)
-    rad%wrk_ir%rz_ir = RadiateZWrk(nz)
+    rad%wrk_ir%rx = RadiateXSWrk(rad%ir, nz)
+    rad%wrk_ir%rz = RadiateZWrk(nz)
     allocate(rad%wrk_ir%fup_a(nz+1, rad%ir%nw))
     allocate(rad%wrk_ir%fdn_a(nz+1, rad%ir%nw))
     allocate(rad%wrk_ir%fup_n(nz+1))
@@ -142,13 +169,13 @@ contains
     type(ClimaRadtranWrk), pointer :: wrk 
     
     wrk => self%wrk_ir                                           
-    call check_dimensions(self, T, P, densities, dz, err)
+    call check_dimensions(self%nz, self%ng, T, P, densities, dz, err)
     if (allocated(err)) return
                                          
     call radiate(self%ir, &
                  0.0_dp, 0.0_dp, 0.0_dp, [0.0_dp], &
                  P, T_surface, T, densities, dz, &
-                 wrk%rx_ir, wrk%rz_ir, &
+                 wrk%rx, wrk%rz, &
                  wrk%fup_a, wrk%fdn_a, wrk%fup_n, wrk%fdn_n)
     
   end subroutine
@@ -171,8 +198,8 @@ contains
     
   end function
   
-  subroutine check_dimensions(self, T, P, densities, dz, err)
-    class(RadtranIR), intent(inout) :: self
+  subroutine check_dimensions(nz, ng, T, P, densities, dz, err)
+    integer, intent(in) :: nz, ng
     real(dp), intent(in) :: T(:) !! (nz) Temperature (K) 
     real(dp), intent(in) :: P(:) !! (nz) Pressure (bars)
     real(dp), intent(in) :: densities(:,:) !! (nz,ng) number density of each 
@@ -180,25 +207,165 @@ contains
     real(dp), intent(in) :: dz(:) !! (nz) thickness of each layer (cm)
     character(:), allocatable, intent(out) :: err
     
-    if (size(T) /= self%nz) then
+    if (size(T) /= nz) then
       err = '"T" has the wrong input dimension.'
       return
     endif
-    if (size(P) /= self%nz) then
+    if (size(P) /= nz) then
       err = '"P" has the wrong input dimension.'
       return
     endif
-    if (size(densities,1) /= self%nz .or. size(densities,2) /= self%ng) then
+    if (size(densities,1) /= nz .or. size(densities,2) /= ng) then
       err = '"densities" has the wrong input dimension.'
       return
     endif
-    if (size(dz) /= self%nz) then
+    if (size(dz) /= nz) then
       err = '"dz" has the wrong input dimension.'
       return
     endif
     
   end subroutine
 
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  !!! IR and Solar Radiative Transfer !!!
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  function create_Radtran_1(datadir, settings_f, star_f, solar_zenith, surface_albedo, nz, err) result(rad)
+    use clima_types, only: ClimaSettings
+    
+    character(*), intent(in) :: datadir
+    character(*), intent(in) :: settings_f
+    character(*), intent(in) :: star_f
+    real(dp), intent(in) :: solar_zenith, surface_albedo
+    integer, intent(in) :: nz
+    character(:), allocatable, intent(out) :: err
+    
+    type(Radtran) :: rad
+    
+    type(ClimaSettings) :: s
+    
+    s = ClimaSettings(settings_f, err)
+    if (allocated(err)) return
+    
+    if (.not. allocated(s%species)) then
+      err = '"'//settings_f//'/optical-properties/species" does not exist'
+      return
+    endif
+    
+    rad = create_Radtran_2(datadir, s%species, s, star_f, solar_zenith, surface_albedo, nz, err)
+    if (allocated(err)) return
+    
+  end function
+  
+  function create_Radtran_2(datadir, species_names, s, star_f, solar_zenith, surface_albedo, nz, err) result(rad)
+    use clima_radtran_types, only: FarUVOpticalProperties, SolarOpticalProperties, IROpticalProperties, &
+                                   read_stellar_flux
+    use clima_types, only: ClimaSettings
+    
+    character(*), intent(in) :: datadir
+    character(*), intent(in) :: species_names(:)
+    type(ClimaSettings), intent(in) :: s
+    character(*), intent(in) :: star_f
+    real(dp), intent(in) :: solar_zenith, surface_albedo
+    integer, intent(in) :: nz
+    character(:), allocatable, intent(out) :: err
+    
+    type(Radtran) :: rad
+    
+    if (nz < 1) then
+      err = '"nz" can not be less than 1.'
+      return
+    endif
+    
+    rad%ng = size(species_names)
+    rad%species_names = species_names
+    rad%nz = nz
+
+    rad%solar_zenith = solar_zenith
+    rad%surface_albedo = surface_albedo
+    
+    if (.not. allocated(s%ir)) then
+      err = '"'//s%filename//'/optical-properties/ir" does not exist.'
+      return
+    endif
+    rad%ir = OpticalProperties(datadir, IROpticalProperties, species_names, s%ir, err)
+    if (allocated(err)) return
+
+    if (.not. allocated(s%sol)) then
+      err = '"'//s%filename//'/optical-properties/solar" does not exist.'
+      return
+    endif
+    rad%sol = OpticalProperties(datadir, SolarOpticalProperties, species_names, s%sol, err)
+    if (allocated(err)) return
+
+    ! photons hitting the planet
+    allocate(rad%photons_sol(rad%sol%nw))
+    call read_stellar_flux(star_f, rad%sol%nw, rad%sol%wavl, rad%photons_sol, err)
+    if (allocated(err)) return
+
+    ! IR work arrays
+    rad%wrk_ir%rx = RadiateXSWrk(rad%ir, nz)
+    rad%wrk_ir%rz = RadiateZWrk(nz)
+    allocate(rad%wrk_ir%fup_a(nz+1, rad%ir%nw))
+    allocate(rad%wrk_ir%fdn_a(nz+1, rad%ir%nw))
+    allocate(rad%wrk_ir%fup_n(nz+1))
+    allocate(rad%wrk_ir%fdn_n(nz+1))
+
+    ! Solar work arrays
+    rad%wrk_sol%rx = RadiateXSWrk(rad%sol, nz)
+    rad%wrk_sol%rz = RadiateZWrk(nz)
+    allocate(rad%wrk_sol%fup_a(nz+1, rad%sol%nw))
+    allocate(rad%wrk_sol%fdn_a(nz+1, rad%sol%nw))
+    allocate(rad%wrk_sol%fup_n(nz+1))
+    allocate(rad%wrk_sol%fdn_n(nz+1))
+
+    ! total flux
+    allocate(rad%f_total(nz+1))
+
+  end function
+
+  subroutine Radtran_radiate(self, T_surface, T, P, densities, dz, err)
+    use clima_radtran_radiate, only: radiate
+    use clima_const, only: pi
+    class(Radtran), target, intent(inout) :: self
+    real(dp), intent(in) :: T_surface
+    real(dp), intent(in) :: T(:) !! (nz) Temperature (K) 
+    real(dp), intent(in) :: P(:) !! (nz) Pressure (bars)
+    real(dp), intent(in) :: densities(:,:) !! (nz,ng) number density of each 
+                                           !! molecule in each layer (molcules/cm3)
+    real(dp), intent(in) :: dz(:) !! (nz) thickness of each layer (cm)
+    character(:), allocatable, intent(out) :: err
+    
+    real(dp) :: u0
+
+    type(ClimaRadtranWrk), pointer :: wrk_ir
+    type(ClimaRadtranWrk), pointer :: wrk_sol
+    
+    wrk_ir => self%wrk_ir
+    wrk_sol => self%wrk_sol                                        
+    call check_dimensions(self%nz, self%ng, T, P, densities, dz, err)
+    if (allocated(err)) return
+
+    ! IR radiative transfer                                     
+    call radiate(self%ir, &
+                 0.0_dp, 0.0_dp, 0.0_dp, [0.0_dp], &
+                 P, T_surface, T, densities, dz, &
+                 wrk_ir%rx, wrk_ir%rz, &
+                 wrk_ir%fup_a, wrk_ir%fdn_a, wrk_ir%fup_n, wrk_ir%fdn_n)
+    
+    ! Solar radiative transfer
+    u0 = cos(self%solar_zenith*pi/180.0_dp)
+    call radiate(self%sol, &
+                 self%surface_albedo, u0, self%diurnal_fac, self%photons_sol, &
+                 P, T_surface, T, densities, dz, &
+                 wrk_sol%rx, wrk_sol%rz, &
+                 wrk_sol%fup_a, wrk_sol%fdn_a, wrk_sol%fup_n, wrk_sol%fdn_n)
+    
+    ! Total flux at edges of layers (ergs/(cm2 s) which is the same as mW/m2).
+    ! Index 1 is bottom. Index nz+1 is top edge of top layer.
+    self%f_total = (wrk_sol%fup_n - wrk_sol%fdn_n) + (wrk_ir%fup_n - wrk_ir%fdn_n)
+
+  end subroutine
   
 end module
   
--- tests/test_clima.f90
+++ tests/test_clima.f90
@@ -1,60 +1,139 @@
 
 program test_clima
-  use clima_radtran, only: RadtranIR, dp, RadtranIR_OLR
-  use clima_types, only: AtmosphereFile, unpack_atmospherefile
-  use clima_eqns, only: vertical_grid
-  use clima_const, only: k_boltz
   implicit none
 
-  type(RadtranIR) :: rad
-  type(AtmosphereFile) :: atm
-  character(:), allocatable :: err
-  
-  integer :: nz, i
-  real(dp) :: OLR
-  real(dp), allocatable :: T(:), P(:), densities(:,:), mix(:,:), dz(:), z(:), density(:)
-  
-  nz = 200
-  
-  rad = RadtranIR("../data","../settings.yaml",nz, err)
-  if (allocated(err)) then
-    print*,err
-    stop 1
-  endif
-  
-  ! step up the atmosphere
-  allocate(T(nz), P(nz), densities(nz,rad%ng), mix(nz,rad%ng), dz(nz), z(nz), density(nz))
-  
-  atm = AtmosphereFile("../atmosphere.txt", err)
-  if (allocated(err)) then
-    print*,err
-    stop 1
-  endif
-  
-  call vertical_grid(0.0_dp, 1.0e7_dp, nz, z, dz)
-  call unpack_atmospherefile(atm, rad%species_names, z, mix, T, P, err)
-  if (allocated(err)) then
-    print*,err
-    stop 1
-  endif
-  
-  density = (P*1.0e6_dp)/(k_boltz*T)
-  do i = 1,rad%ng
-    densities(:,i) = mix(:,i)*density
-  enddo
-  
-  OLR = RadtranIR_OLR(rad, 289.0_dp, T, P, densities, dz, err)
-  if (allocated(err)) then
-    print*,err
-    stop 1
-  endif
-  
-  print*,OLR
-  
-  open(unit=1,file='../OLR.dat',form='unformatted',status='replace')
-  write(1) 0.5_dp*(rad%ir%freq(1:rad%ir%nw)+rad%ir%freq(2:rad%ir%nw+1))
-  write(1) rad%wrk_ir%fup_a(nz+1,:)
-  close(1)
-  
+  call test_RadtranIR()
+  call test_Radtran()
 
+contains
+
+  subroutine test_RadtranIR()
+    use clima_radtran, only: RadtranIR
+    use clima_types, only: AtmosphereFile, unpack_atmospherefile
+    use clima_eqns, only: vertical_grid
+    use clima_const, only: k_boltz, dp
+    implicit none
+  
+    type(RadtranIR) :: rad
+    type(AtmosphereFile) :: atm
+    character(:), allocatable :: err
+    
+    integer :: nz, i
+    real(dp) :: OLR
+    real(dp), allocatable :: T(:), P(:), densities(:,:), mix(:,:), dz(:), z(:), density(:)
+    
+    atm = AtmosphereFile("../atmosphere.txt", err)
+    if (allocated(err)) then
+      print*,err
+      stop 1
+    endif
+  
+    nz = size(atm%columns(1,:))
+    
+    rad = RadtranIR("../data","../settings.yaml",nz, err)
+    if (allocated(err)) then
+      print*,err
+      stop 1
+    endif
+    
+    ! step up the atmosphere
+    allocate(T(nz), P(nz), densities(nz,rad%ng), mix(nz,rad%ng), dz(nz), z(nz), density(nz))
+    
+    call vertical_grid(0.0_dp, 1.0e7_dp, nz, z, dz)
+    call unpack_atmospherefile(atm, rad%species_names, z, mix, T, P, err)
+    if (allocated(err)) then
+      print*,err
+      stop 1
+    endif
+    
+    density = (P*1.0e6_dp)/(k_boltz*T)
+    do i = 1,rad%ng
+      densities(:,i) = mix(:,i)*density
+    enddo
+    
+    OLR = rad%OLR(289.0_dp, T, P, densities, dz, err)
+    if (allocated(err)) then
+      print*,err
+      stop 1
+    endif
+    
+    print*,OLR
+    
+    open(unit=1,file='../OLR.dat',form='unformatted',status='replace')
+    write(1) 0.5_dp*(rad%ir%freq(1:rad%ir%nw)+rad%ir%freq(2:rad%ir%nw+1))
+    write(1) rad%wrk_ir%fup_a(nz+1,:)
+    close(1)
+
+  end subroutine
+
+  subroutine test_Radtran()
+    use clima_radtran, only: Radtran
+    use clima_types, only: AtmosphereFile, unpack_atmospherefile
+    use clima_eqns, only: vertical_grid
+    use clima_const, only: k_boltz, dp
+    implicit none
+  
+    type(Radtran) :: rad
+    type(AtmosphereFile) :: atm
+    character(:), allocatable :: err
+    
+    integer :: nz, i
+    real(dp) :: solar_zenith, surface_albedo, T_shift
+    real(dp), allocatable :: T(:), P(:), densities(:,:), mix(:,:), dz(:), z(:), density(:)
+    
+    atm = AtmosphereFile("../atmosphere.txt", err)
+    if (allocated(err)) then
+      print*,err
+      stop 1
+    endif
+  
+    nz = size(atm%columns(1,:))
+
+    solar_zenith = 60.0_dp
+    surface_albedo = 0.15_dp
+    rad = Radtran("../data","../settings.yaml","../Sun_now.txt", solar_zenith, surface_albedo, nz, err)
+    if (allocated(err)) then
+      print*,err
+      stop 1
+    endif
+    ! rad%diurnal_fac = 1.0_dp
+    
+    ! step up the atmosphere
+    allocate(T(nz), P(nz), densities(nz,rad%ng), mix(nz,rad%ng), dz(nz), z(nz), density(nz))
+    
+    call vertical_grid(0.0_dp, 1.0e7_dp, nz, z, dz)
+    call unpack_atmospherefile(atm, rad%species_names, z, mix, T, P, err)
+    if (allocated(err)) then
+      print*,err
+      stop 1
+    endif
+
+    T_shift = 0.0_dp
+    T = T + T_shift
+    
+    density = (P*1.0e6_dp)/(k_boltz*T)
+    do i = 1,rad%ng
+      densities(:,i) = mix(:,i)*density
+    enddo
+    
+    call rad%radiate(T(1), T, P, densities, dz, err)
+    if (allocated(err)) then
+      print*,err
+      stop 1
+    endif
+
+    print*,rad%wrk_sol%fdn_n(nz+1)*1.0e-3_dp
+
+    open(unit=1,file='../f_total.dat',form='unformatted',status='replace')
+    write(1) z
+    write(1) dz
+    write(1) rad%f_total(:)
+    write(1) rad%wrk_sol%fup_n(:)
+    write(1) rad%wrk_sol%fdn_n(:)
+    write(1) rad%wrk_ir%fup_n(:)
+    write(1) rad%wrk_ir%fdn_n(:)
+    close(1)
+
+  end subroutine
+  
 end program
