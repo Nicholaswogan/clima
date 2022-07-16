2a926805ec17d54d7e52f98afdf4b96e349f361a
Nick Wogan <nicholaswogan@gmail.com>
2022-07-15 17:29:50 -0700

    implemented mixing length theory. included the ground. Modern Earth looks pretty decent

--- src/climate/clima_climate_rhs.f90
+++ src/climate/clima_climate_rhs.f90
@@ -19,12 +19,16 @@ contains
     real(dp) :: adiabat_lapse(self%nz)
     real(dp) :: scale_height(self%nz)
     real(dp) :: rho(self%nz)
-    real(dp) :: Fc_e(self%nz-1)
+    real(dp) :: Fc_e(self%nz-1), Fc_g
     real(dp) :: Latent_heat(self%nz)
     real(dp) :: dTdt_l(self%nz)
     real(dp) :: dFdz(self%nz)
     
     real(dp) :: delta_z
+
+    real(dp), parameter :: cp_ground = 4.182e7_dp ! H2O, erg/(g*K)
+    real(dp), parameter :: rho_ground = 1.0_dp ! H2O, g/cm3
+    real(dp), parameter :: dz_ground = 500.0_dp ! cm
 
     type(ClimateWrk), pointer :: w
     
@@ -94,8 +98,8 @@ contains
     scale_height(:) = (k_boltz*w%T(:)*N_avo)/(self%mubar(:)*self%grav(:))
     
     ! Convection
-    call convection_diffusion(w%T(:), self%grav, self%z, self%dz, cp, &
-                              rho, adiabat_lapse, scale_height, Fc_e)
+    call convection_diffusion(w%T(:), w%T_surf, self%grav, self%z, self%dz, dz_ground, cp, cp_ground, &
+                              rho, rho_ground, adiabat_lapse, scale_height, Fc_e, Fc_g)
     
     ! Latent heating (ergs/(cm2 s))
     do j = 1,self%nz
@@ -105,7 +109,7 @@ contains
       ! enddo 
     enddo
 
-    Fc_e(:) = 0.0_dp
+    ! Fc_e(:) = 0.0_dp
 
     if (self%double_radiative_grid) then
       do i = 1,self%nz
@@ -128,8 +132,8 @@ contains
     ! lower layer
     j = 1
     dTdt_l(j) = (1.0_dp/(rho(j)*cp(j)))*dFdz(j) + &
-              (-1.0_dp/(rho(j)*cp(j)))*(Fc_e(1)/self%dz(j) - 0.0_dp) + &
-              (1.0_dp/(rho(j)*cp(j)))*Latent_heat(j)
+                (-1.0_dp/(rho(j)*cp(j)))*(Fc_e(1) - Fc_g)/self%dz(j) + &
+                (1.0_dp/(rho(j)*cp(j)))*Latent_heat(j)
         
     ! upper boundary
     j = self%nz
@@ -138,30 +142,32 @@ contains
               (1.0_dp/(rho(j)*cp(j)))*Latent_heat(j)
     
     ! ground level
-    dTdt(1) = (1.0_dp/(1.0_dp*4.182e7_dp))*(self%rad%f_total(1))/(500.0_dp)
+    dTdt(1) = (1.0_dp/(rho_ground*cp_ground))*(self%rad%f_total(1))/dz_ground + &
+              (-1.0_dp/(rho_ground*cp_ground))*(Fc_g/dz_ground - 0.0_dp) 
 
     ! append atmosphere
     dTdt(2:) = dTdt_l
       
   end subroutine
   
-  subroutine convection_diffusion(T, grav, z, dz, cp, rho, adiabat_lapse, scale_height, Fc_e)
+  subroutine convection_diffusion(T, T_surf, grav, z, dz, dz_ground, cp, cp_ground, rho, rho_ground, &
+                                  adiabat_lapse, scale_height, Fc_e, Fc_g)
     use clima_const, only: von_karman_const
     use clima_eqns, only: eddy_for_heat
-    real(dp), intent(in) :: T(:)
+    real(dp), intent(in) :: T(:), T_surf
     real(dp), intent(in) :: grav(:)
     real(dp), intent(in) :: z(:)
-    real(dp), intent(in) :: dz(:)
-    real(dp), intent(in) :: cp(:)
-    real(dp), intent(in) :: rho(:)
+    real(dp), intent(in) :: dz(:), dz_ground
+    real(dp), intent(in) :: cp(:), cp_ground
+    real(dp), intent(in) :: rho(:), rho_ground
     real(dp), intent(in) :: adiabat_lapse(:)
     real(dp), intent(in) :: scale_height(:)
     
     ! convection on the edges
-    real(dp), intent(out) :: Fc_e(:)
+    real(dp), intent(out) :: Fc_e(:), Fc_g
     
     ! heat diffusion coefficient on all inner edges
-    real(dp) :: Kh_e(size(T)-1)
+    real(dp) :: Kh_e(size(T)-1), Kh_g
     ! mixing length (cm)
     real(dp) :: free_length_prop_const
     real(dp) :: free_mixing_length(size(T))
@@ -198,7 +204,23 @@ contains
       Fc_e(i) = - (rho_av*cp_av*Kh_e(i))*(dTdz + adiabat_av)
 
     enddo
- 
+
+    ! surface layer 
+    ! (layer between the ground and the first atmospheric layer)
+    rho_av = sqrt(rho_ground*rho(1))
+    cp_av = sqrt(cp_ground*cp(1))
+    grav_av = grav(i)
+    T_av = sqrt(T_surf*T(1))
+    adiabat_av = adiabat_lapse(1)
+    mixing_length_av = mixing_length(1)
+
+    delta_z = dz_ground/2.0_dp + dz(1)/2.0_dp 
+
+    dTdz = (T(1)-T_surf)/delta_z
+
+    Kh_g = eddy_for_heat(mixing_length_av, grav_av, T_av, dTdz, adiabat_av)
+    Fc_g = - (rho_av*cp_av*Kh_g)*(dTdz + adiabat_av)
+
   end subroutine
   
 end submodule
--- tests/test_climate.f90
+++ tests/test_climate.f90
@@ -31,7 +31,7 @@ contains
     allocate(t_eval(100))
     t_eval = 10.0_dp**linspace(1.0_dp,10.0_dp,100)
 
-    c%T_init = 250.0_dp
+    ! c%T_init = 250.0_dp
 
     success = c%evolve("../test1.dat", 0.0_dp, c%T_init, t_eval, .true., err) 
     if (allocated(err)) then
