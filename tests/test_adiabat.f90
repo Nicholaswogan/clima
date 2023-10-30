214d6cfe142b392b5004d8d818167d9f2ccbd80e
Nick Wogan <nicholaswogan@gmail.com>
2023-10-30 09:32:57 -0700

    added species to default example

--- tests/test_adiabat.f90
+++ tests/test_adiabat.f90
@@ -7,6 +7,7 @@ program test
   type(AdiabatClimate) :: c
   character(:), allocatable :: err
   real(dp) :: T, OLR
+  real(dp), allocatable :: P_i_surf(:), N_i_surf(:)
   integer :: i
   procedure(ocean_solubility_fcn), pointer :: ocean_fcn_ptr
   
@@ -20,56 +21,64 @@ program test
     stop 1
   endif
 
+  ! Test tidally locked dayside
   c%solve_for_T_trop = .true.
   c%tidally_locked_dayside = .true.
-    
+  P_i_surf = [270.0_dp, 400e-6_dp, 1.0_dp, 1.0e-10_dp, 1.0e-10_dp, 1.0e-10_dp, 1.0e-10_dp]
+  P_i_surf = P_i_surf*1.0e6_dp
   T = c%surface_temperature( &
-      [270.0e6_dp, 400e-6_dp*1.0e6_dp, 1.0e6_dp], &
+      P_i_surf, &
       T_guess = 280.0_dp, err=err)
   if (allocated(err)) then
     print*,err
     stop 1
   endif
-
   print*,T, c%T_trop
+
+  ! Test surface_temperature_column
   c%tidally_locked_dayside = .false.
-
+  N_i_surf = [15.0e3_dp, 400e-6_dp*23.0_dp, 1.0*36.0_dp, 1.0e-10_dp, 1.0e-10_dp, 1.0e-10_dp, 1.0e-10_dp]
   T = c%surface_temperature_column( &
-      [15.0e3_dp, 400e-6_dp*23.0_dp, 1.0*36.0_dp], &
+      N_i_surf, &
       T_guess = 280.0_dp, err=err)
   if (allocated(err)) then
     print*,err
     stop 1
   endif
-
   print*,T, c%T_trop
 
+  ! Test surface_temperature_bg_gas
+  P_i_surf = [270.0_dp, 400e-6_dp, 1.0_dp, 1.0e-10_dp, 1.0e-10_dp, 1.0e-10_dp, 1.0e-10_dp]
+  P_i_surf = P_i_surf*1.0e6_dp
   T = c%surface_temperature_bg_gas( &
-      [270.0e6_dp, 400e-6_dp*1.0e6_dp, 1.0e6_dp], &
+      P_i_surf, &
       P_surf = 1.0e6_dp, bg_gas='N2', T_guess = 280.0_dp, err=err)
   if (allocated(err)) then
     print*,err
     stop 1
   endif
-
   print*,T, c%T_trop
 
+  ! Test to_regular_grid
   call c%to_regular_grid(err)
   if (allocated(err)) then
     print*,err
     stop 1
   endif
 
-  ! A case where CO2 will also begin condensing
+  ! Test a case where CO2 begins to condense
   c%T_trop = 150.0_dp
+  P_i_surf = [270.0_dp, 10.0_dp, 1.0_dp, 1.0e-10_dp, 1.0e-10_dp, 1.0e-10_dp, 1.0e-10_dp]
+  P_i_surf = P_i_surf*1.0e6_dp
   call c%make_profile(280.0_dp, &
-      [270.0e6_dp, 10.0e6_dp, 1.0e6_dp], &
+      P_i_surf, &
       err=err)
   if (allocated(err)) then
     print*,err
     stop 1
   endif
 
+  ! Test ocean solubility functionality
   ocean_fcn_ptr => ocean_fcn
   call c%set_ocean_solubility_fcn('H2O', ocean_fcn_ptr, err)
   if (allocated(err)) then
@@ -78,9 +87,9 @@ program test
   endif
 
   c%use_make_column_P_guess = .true.
-
+  N_i_surf = [15.0e4_dp, 400e-6_dp*23.0_dp, 1.0*36.0_dp, 1.0e-10_dp, 1.0e-10_dp, 1.0e-10_dp, 1.0e-10_dp]
   call c%make_column(280.0_dp, &
-      [15.0e4_dp, 400e-6_dp*23.0_dp, 1.0*36.0_dp], &
+      N_i_surf, &
       err=err)
   if (allocated(err)) then
     print*,err
@@ -88,8 +97,10 @@ program test
   endif
 
   print*,c%N_surface+c%N_atmos+c%N_ocean(:,1)
-  print*,15.0e3_dp, 400e-6_dp*23.0_dp, 1.0*36.0_dp
+  print*,N_i_surf
 
+  ! deallocate
+  deallocate(P_i_surf, N_i_surf)
 contains
 
   subroutine ocean_fcn(T_surf, ng, P_i, m_i, args_p) 
@@ -99,7 +110,7 @@ contains
     real(c_double), intent(in) :: P_i(ng) !! bars
     real(c_double), intent(out) :: m_i(ng) !! mol/kg
     type(c_ptr), value, intent(in) :: args_p
-    m_i(1) = 0.0_dp
+    m_i(:) = 0.0_dp
     m_i(2) = P_i(2)*3.4e-2_dp
     m_i(3) = P_i(3)*6.1e-4_dp
   end subroutine
